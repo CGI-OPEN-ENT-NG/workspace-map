d82483df4dcf6f050f3aa3ce983faa975eccc078
Simon Ledunois <simon.ledunois@cgi.com>
2025-09-01 16:30:40 +0200

    feat: map v3

---
+++ tilesets/Office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Office" tilewidth="32" tileheight="32" tilecount="848" columns="16">
+ <image source="Office.png" width="512" height="1696"/>
+</tileset>
